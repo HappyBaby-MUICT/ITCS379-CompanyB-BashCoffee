7aa9b78fe064ecab5799dbcfcf23cfc05d02fbcd
Pattanun W <notzkrran2020@gmail.com>
2024-11-24 17:37:16 +0700

    fix: package issue

--- packages/line-db/src/index.ts
+++ packages/line-db/src/index.ts
@@ -1,6 +1,6 @@
 import { PrismaModule } from './prisma.module'
 import { PrismaService } from './prisma.service'
-import { PrismaClient, Prisma as PrismaTypes } from '../liff/dist/liff/client'
-import type * as PrismaModels from '../liff/dist/liff/client'
+import { PrismaClient, Prisma as PrismaTypes } from '../dist/liff/client'
+import type * as PrismaModels from '../dist/liff/client'
 
 export { PrismaTypes, PrismaClient, PrismaModule, PrismaService, PrismaModels }
